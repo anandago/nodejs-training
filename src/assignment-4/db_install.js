8657f4fc791593e467620c79d526cfac0c5198fa
Aravindan N <aravindan_nandagopalan@mindtree.com>
2018-06-16 22:46:37 +0530

    Adding mysql in circleci configuration. --testing

--- src/assignment-4/db_install.js
+++ src/assignment-4/db_install.js
@@ -4,6 +4,7 @@ const con = mysql.createConnection({
   host: process.env.MYSQL_ADMIN_HOST || "127.0.0.1",
   port: process.env.MYSQL_ADMIN_PORT || 3306,
   user: process.env.MYSQL_ADMIN_USER || "root",
+  // password: process.env.MYSQL_ADMIN_PASSWORD || "root"
 });
 
 const USERS = "CREATE TABLE IF NOT EXISTS users (userid SMALLINT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(10) NOT NULL, password VARCHAR(10) NOT NULL, status BIT(2) NOT NULL);";
@@ -13,6 +14,13 @@ con.connect((err) => {
   if (err) throw console.error('error connecting: ' + err.stack);
 
   console.log(`Connected id: ${con.threadId}`);
+});
+
+// Show Innodb status.
+con.query("SHOW ENGINE INNODB STATUS", (err, result) => {
+  if (err) throw console.error(err.stack);
+
+  console.log("INNODB Status", result);
 });
 
 // Create new database and use that.
