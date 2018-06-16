7cb8c34f2c2dcc481d1716b2512d5a3565f558dd
Aravindan N <aravindan_nandagopalan@mindtree.com>
2018-06-17 00:15:13 +0530

    Lint fixes

--- src/assignment-4/db.js
+++ src/assignment-4/db.js
@@ -1,12 +1,14 @@
+'use strict';
+
 const mysql = require('mysql');
 
-const pool  = mysql.createPool({
-  connectionLimit : 10,
-  host: process.env.MYSQL_HOST || "127.0.0.1",
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.MYSQL_HOST || '127.0.0.1',
   port: process.env.MYSQL_PORT || 3306,
-  user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "root",
-  database: process.env.MYSQL_DB || "leave_the_marks"
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || 'root',
+  database: process.env.MYSQL_DB || 'leave_the_marks',
 });
 
-module.exports = pool
+module.exports = pool;
--- src/assignment-4/db_install.js
+++ src/assignment-4/db_install.js
@@ -1,14 +1,16 @@
+'use strict';
+
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-  host: process.env.MYSQL_ADMIN_HOST || "127.0.0.1",
+  host: process.env.MYSQL_ADMIN_HOST || '127.0.0.1',
   port: process.env.MYSQL_ADMIN_PORT || 3306,
-  user: process.env.MYSQL_ADMIN_USER || "root",
-  // password: process.env.MYSQL_ADMIN_PASSWORD || "root"
+  user: process.env.MYSQL_ADMIN_USER || 'root',
+  // password: process.env.MYSQL_ADMIN_PASSWORD || 'root'
 });
 
-const USERS = "CREATE TABLE IF NOT EXISTS users (userid SMALLINT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(10) NOT NULL, password VARCHAR(10) NOT NULL, status BIT(2) NOT NULL);";
-const STORIES = "CREATE TABLE IF NOT EXISTS stories (storyid SMALLINT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, story VARCHAR(30) NOT NULL, userid SMALLINT(6) UNSIGNED NOT NULL, INDEX (userid), FOREIGN KEY (userid) REFERENCES users(userid));";
+const USERS = 'CREATE TABLE IF NOT EXISTS users (userid SMALLINT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(10) NOT NULL, password VARCHAR(10) NOT NULL, status BIT(2) NOT NULL);';
+const STORIES = 'CREATE TABLE IF NOT EXISTS stories (storyid SMALLINT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, story VARCHAR(30) NOT NULL, userid SMALLINT(6) UNSIGNED NOT NULL, INDEX (userid), FOREIGN KEY (userid) REFERENCES users(userid));';
 
 con.connect((err) => {
   if (err) throw console.error('error connecting: ' + err.stack);
@@ -17,27 +19,27 @@ con.connect((err) => {
 });
 
 // Create new database and use that.
-con.query("CREATE DATABASE IF NOT EXISTS leave_the_marks", (err, result) => {
+con.query('CREATE DATABASE IF NOT EXISTS leave_the_marks', (err, result) => {
   if (err) throw console.error(err.stack);
 
-  console.log("Database created");
+  console.log('Database created');
 });
 
-con.query("USE leave_the_marks", (err, result) => {
+con.query('USE leave_the_marks', (err, result) => {
   if (err) throw console.error(err.stack);
 
-  console.log("Using database leave_the_marks");
+  console.log('Using database leave_the_marks');
 });
 
 con.query(USERS, (err, result) => {
   if (err) throw console.error(err.stack);
-  console.log("User table created");
+  console.log('User table created');
 });
 
 con.query(STORIES, (err, result) => {
   if (err) throw console.error(err.stack);
 
-  console.log("Story table created");
+  console.log('Story table created');
 });
 
 con.end(function(err) {
